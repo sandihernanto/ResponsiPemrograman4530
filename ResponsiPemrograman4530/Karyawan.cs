6da132ded15d9f58a7571d13b0d593621d343a19
ACER <ACER@LAPTOP-FQJJHO85>
2022-04-21 09:58:36 +0700

    Add project files.

---
+++ ResponsiPemrograman4530/Karyawan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4530
+{
+    internal class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; } 
+        public int gajinaik { get; set; }   
+        
+        public Karyawan (int Nik,string Nama,int GajiBulanan)
+        {
+            Nik = Nik;
+            Nama = Nama;
+            GajiBulanan = GajiBulanan;
+
+            gajinaik = 10;
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }    
+        }
+        public void getdapatbonus()
+        {
+            int bonus = GajiBulanan / gajinaik;
+            GajiBulanan = GajiBulanan + bonus;
+            Console.WriteLine("{0} \t{1}\t\t\t {2}", Nik,Nama,GajiBulanan);
+        }
+        public void PrintAndShow()
+        {
+            Console.WriteLine("{0}  \t{1}\t\t\t {2}", Nik, Nama, GajiBulanan);
+
+        }
+    }
+}
---
+++ ResponsiPemrograman4530/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4530
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nik\t\t\t Nama \t\t\t Gaji Bulanan");
+   
+
+
+            Karyawan karyawan1 = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "Jono", 2000000);
+
+            karyawan1.Nik = 190302123;
+            karyawan2.Nik = 190302124;
+            karyawan1.Nama = "Paijo";
+            karyawan2.Nama = "Jono";
+            karyawan1.GajiBulanan = 3000000;
+            karyawan2.GajiBulanan= 2000000;
+            
+
+            karyawan1.PrintAndShow();
+            karyawan2.PrintAndShow();
+
+            Console.WriteLine("\n\n Asyiiik kenaikan gaji 10% :)");
+            Console.WriteLine("\n\nNik\t\t\t Nama \t\t\t Gaji");
+          
+            karyawan1.Nik = 190302123;
+            karyawan2.Nik = 190302124;
+            karyawan1.Nama = "Paijo";
+            karyawan2.Nama = "Jono";
+            karyawan1.GajiBulanan = 3000000;
+            karyawan2.GajiBulanan = 2000000;
+
+
+            karyawan1.getdapatbonus();
+            karyawan2.getdapatbonus();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
